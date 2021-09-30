c0e858dfc51a0e5dc38584bc8dff43281c86ad27
ISedykh <isedykh@sportmaster.ru>
2021-09-30 17:35:13 +0300

    added webpack, plugins, loaders, babel

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './scss/styles.scss';
+
+const c = 'test';
+
+const obj1 = {
+  name: 'Ivan',
+};
+
+const obj2 = {
+  age: 20,
+};
+
+const info = { ...obj1, ...obj2 };
+
+console.log(c);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'), // обсалютный путь до папки src, берётся за точку отсчёта
+  entry: {
+    main: ['@babel/polyfill', './index.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    // расширения подключаемых файлов
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src', 'core'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        // Порядок выполнения справа налево (sass-loader -> css-loader -> Mini...)
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
