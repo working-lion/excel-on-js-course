35fd160db81b9842be92f11e399d73e900ee0a8b
ISedykh <isedykh@sportmaster.ru>
2021-10-01 12:23:45 +0300

    added dev and prod configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,58 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'), // обсалютный путь до папки src, берётся за точку отсчёта
-  entry: {
-    main: './index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-  },
-  resolve: {
-    // расширения подключаемых файлов
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src', 'core'),
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  const isDev = !isProd;
+
+  const getFileName = (ext) => (isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`);
+
+  return {
+    context: path.resolve(__dirname, 'src'), // обсалютный путь до папки src, берётся за точку отсчёта
+    entry: {
+      main: './index.js',
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: getFileName('js'),
+    },
+    resolve: {
+      // расширения подключаемых файлов
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist'),
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: getFileName('css'),
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, 'src', 'favicon.ico'),
-          to: path.resolve(__dirname, 'dist'),
+          test: /\.s[ac]ss$/i,
+          // Порядок выполнения справа налево (sass-loader -> css-loader -> Mini...)
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         },
       ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].bundle.css',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        // Порядок выполнения справа налево (sass-loader -> css-loader -> Mini...)
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
+    },
+    devtool: isDev ? 'source-map' : false,
+  };
 };
